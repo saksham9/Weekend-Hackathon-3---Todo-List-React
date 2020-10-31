aacfda69b4891c5df9960c56b0f1470e33fffb3e
saksham9 <agrawal.saksham9@gmail.com>
2020-10-31 21:31:00 +0530

    change

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
 
 function ListItem(props) {
+  const [open, setOpen] = React.useState(false);
   const [task, setTask] = useState("");
   const [checkempty, setCheckempty] = useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handletext = (event) => {
     if (event.target.value !== "") {
       setTask(event.target.value);
@@ -15,18 +26,8 @@ function ListItem(props) {
     <li key={props.key} className="list">
       {props.item}
       <span>
-        <textarea
-          className="editTask"
-          onChange={handletext}
-          value={task}
-        ></textarea>
-        <button
-          className="saveTask"
-          onClick={() => {
-            props.taskChange(props.index, task, checkempty);
-          }}
-        >
-          saveTask
+        <button className="edit" onClick={handleClickOpen}>
+          Edit
         </button>
         <button
           className="delete"
@@ -36,6 +37,30 @@ function ListItem(props) {
         >
           Delete
         </button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {"Edit your task"}
+          </DialogTitle>
+          <textarea
+            className="editTask"
+            onChange={handletext}
+            value={task}
+          ></textarea>
+          <button
+            className="saveTask"
+            onClick={() => {
+              props.taskChange(props.index, task, checkempty);
+              setTask("");
+              handleClose();
+            }}
+          >
+            saveTask
+          </button>
+        </Dialog>
       </span>
     </li>
   );
